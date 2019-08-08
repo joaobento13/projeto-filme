6925bea3175ffda0dc1437a62e54f8bebdf3848c
Joao Victor <joaovictorb00@gmail.com>
2019-08-07 22:24:49 -0300

    add static configuration

--- src/components/Populares.vue
+++ src/components/Populares.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="body-content" id="Populares">
 
-    <div class="loading container-fluid d-flex justify-content-center align-items-center" v-if="loading">
+    <div class="loading container-fluid" v-if="loading">
       <img src="../assets/images/loader.gif" alt="Loader"/>
     </div>
 
@@ -11,7 +11,12 @@
         <div class="row h-100">
           <div class="col-sm-2"></div>
           <div class="search-wrapper col-sm-6 p-0 d-flex align-items-center">
-            <input v-model="search" class="search-input" type="text" placeholder="Procurar por um filme..."/>
+            <input
+              v-model="search"
+              class="search-input"
+              type="text"
+              placeholder="Procurar por um filme..."
+            />
           </div>
         </div>
       </div>
@@ -22,14 +27,18 @@
           <div class="col-sm-8 p-0 pt-3">
             <h1 class="session-title pb-2">{{tituloSession}}</h1>
             <div class="row">
-              <div class="film-content col-sm-6 pb-4" v-for="film in filteredList" v-bind:key="film.id">
+              <div
+                class="film-content col-sm-6 pb-4"
+                v-for="film in filteredList"
+                v-bind:key="film.id"
+              >
                 <div class="row">
                   <div class="col-sm-5">
                     <img class="mw-100" :src="'https://image.tmdb.org/t/p/w200/' + film.poster_path" :alt="film.id"/>
                   </div>
                   <div class="film-info col-sm-7 p-2 pr-3">
                     <div class="film-header d-flex">
-                      <div class="vote_average d-flex justify-content-center align-items-center mr-2 pl-2 pr-2">
+                      <div class="vote_average mr-2 pl-2 pr-2">
                         <span>{{ formatAverage(film.vote_average) }}</span>
                       </div>
                       <div>
@@ -90,30 +99,28 @@ export default {
   created() {
     axios.get('http://www.mocky.io/v2/5d4a06b03200005e00600f5c')
       .then((response) => {
-        console.log('-- response :: ', response);
         this.info = response.data.results;
       })
-      .catch((e) => {
-        console.log('-- error :: ', e);
+      .catch(() => {
         this.errored = true;
       })
-      .finally(() => this.loading = false);
+      .finally(() => {
+        this.loading = false;
+      });
   },
   methods: {
     formatAverage: (value) => {
       let newValue = value.toString();
-      if (newValue.length == 1) {
+      if (newValue.length === 1) {
         newValue += '.0';
       }
       return newValue;
     },
-    formatDate: (value) => {
-      if (value) {
-        return moment(String(value)).format('DD [de] MMMM [de] YYYY');
-      }
-    },
-    formatDescription: (text, length, clamp) => {
-      clamp = clamp || '...';
+    formatDate: value => (
+      moment(String(value)).format('DD [de] MMMM [de] YYYY')
+    ),
+    formatDescription: (text, length, clampParam) => {
+      const clamp = clampParam || '...';
       const node = document.createElement('div');
       node.innerHTML = text;
       const content = node.textContent;
@@ -122,7 +129,9 @@ export default {
   },
   computed: {
     filteredList() {
-      return this.info.filter(film => film.original_title.toLowerCase().includes(this.search.toLowerCase()));
+      return this.info.filter(film =>
+        film.original_title.toLowerCase().includes(this.search.toLowerCase())
+      );
     },
   },
 };
